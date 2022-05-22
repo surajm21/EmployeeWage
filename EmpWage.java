0afb0a18ec6c9a80377996508748d483dbdfa359
suraj <surajmore2143@gmail.com>
2022-05-22 10:52:21 +0530

    Added to check part time employee wage feature

--- EmpWage.java
+++ EmpWage.java
@@ -3,12 +3,15 @@ package com.bridglabz;
 public class EmpWage {
 
     public static void main(String[] args) {
-        int is_Full_Time = 1;
+        int is_Full_Time = 2;
+        int is_PART_Time = 1;
         int EPM_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == is_Full_Time)
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if(empCheck == is_PART_Time)
+            empHrs = 4;
+        else if (empCheck == is_Full_Time)
             empHrs = 8;
         else
             empHrs = 0;
