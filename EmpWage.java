b2064c21b55c8f63ffc0b20d620beba1522d0850
suraj <surajmore2143@gmail.com>
2022-05-22 11:35:52 +0530

    Added to checked employee wage usung switch case feature

--- EmpWage.java
+++ EmpWage.java
@@ -8,13 +8,19 @@ public class EmpWage {
         int EPM_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == is_PART_Time)
-            empHrs = 4;
-        else if (empCheck == is_Full_Time)
-            empHrs = 8;
-        else
-            empHrs = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case 1:
+                System.out.println("Employee is full time present");
+                empHrs = 8;
+                break;
+            case 2:
+                System.out.println("Employee is part time present.");
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
         empWage = empHrs * EPM_RATE_PER_HOUR;
         System.out.println("Emp Wage: "+empWage);
     }
