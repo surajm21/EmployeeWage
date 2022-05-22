2e45e98672a9b5f9a910a339cd7de51a9bdd6f30
suraj <surajmore2143@gmail.com>
2022-05-22 10:25:08 +0530

    Added to check employee is present or Absent feature

---
+++ EmpWage.java
@@ -0,0 +1,13 @@
+package com.bridglabz;
+
+public class EmpWage {
+
+    public static void main(String[] args) {
+        int is_Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == is_Full_Time)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+    }
+}
