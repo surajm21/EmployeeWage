3125d8a524411b8abfab8e467a48dd4542ef9760
suraj <surajmore2143@gmail.com>
2022-05-22 10:43:54 +0530

    Added to check daily employee wage feature

--- EmpWage.java
+++ EmpWage.java
@@ -4,10 +4,15 @@ public class EmpWage {
 
     public static void main(String[] args) {
         int is_Full_Time = 1;
+        int EPM_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if(empCheck == is_Full_Time)
-            System.out.println("Employee is Present");
+            empHrs = 8;
         else
-            System.out.println("Employee is Absent");
+            empHrs = 0;
+        empWage = empHrs * EPM_RATE_PER_HOUR;
+        System.out.println("Emp Wage: "+empWage);
     }
 }
