901ae33d50160a835a797f80ce22b038ca9dac78
suraj <surajmore2143@gmail.com>
2022-05-23 17:50:26 +0530

    Added calculating Wages for a month assuming 20 working days feature

--- EmpWage.java
+++ EmpWage.java
@@ -8,18 +8,28 @@ public class EmpWage {
         int EPM_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
+        int Day = 0;
+        int workingdays = 20;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case 1:
-                System.out.println("Employee is full time present");
-                empHrs = 8;
-                break;
-            case 2:
-                System.out.println("Employee is part time present.");
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        for (Day = 1; Day <= workingdays; Day++){
+
+            switch (empCheck) {
+
+                case 1:
+                    System.out.println("Employee is full time present");
+                    empHrs = 8;
+                    break;
+
+                case 2:
+                    System.out.println("Employee is part time present.");
+                    empHrs = 4;
+                    break;
+
+                default:
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
         }
         empWage = empHrs * EPM_RATE_PER_HOUR;
         System.out.println("Emp Wage: "+empWage);
