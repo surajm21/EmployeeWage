90d0b619553f62213b6aca3f0aa8c2be1629fc6d
suraj <surajmore2143@gmail.com>
2022-06-01 11:01:20 +0530

    Added to save the total wages for each feature

--- EmpWage.java
+++ EmpWage.java
@@ -2,11 +2,12 @@ package com.bridglabz;
 
 public class EmpWage {
     static final int FULL_TIME = 2;
-    static final int IS_PART_TIME = 1;
+    static final int PART_TIME = 1;
     private final String company;
     private final int wagePerHour;
     private final int numOfWorkingDays;
     private final int maxHours;
+    private int totalWage;
 
     public EmpWage(String company, int wagePerHour, int numOfWorkingDays, int maxHours) {
         this.company = company;
@@ -14,38 +15,35 @@ public class EmpWage {
         this.numOfWorkingDays = numOfWorkingDays;
         this.maxHours = maxHours;
     }
-    public int computeEmpWage() {
-        int empHrs = 0, totalEmpHrs = 0, totalWage = 0, totalWorkingDays = 0;
+    public void computeEmpWage() {
+        int empHrs = 0, totalEmpHrs = 0,totalWorkingDays = 0;
         while (numOfWorkingDays > totalWorkingDays && maxHours > totalEmpHrs) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case 1:
-                    System.out.println("Employee is full time present");
-                    empHrs = 8;
-                    break;
-                case 2:
-                    System.out.println("Employee is part time present.");
+                case (PART_TIME):
                     empHrs = 4;
                     break;
-                default:
-                    System.out.println("Employee is Absent");
-                    empHrs = 0;
+                case (FULL_TIME):
+                    empHrs = 8;
                     break;
+                default:
+                    empHrs = 0;
+                    totalWorkingDays++;
             }
-            totalEmpHrs = totalEmpHrs + empHrs;
-            totalWorkingDays++;
+            totalEmpHrs = totalEmpHrs  + empHrs;
+            System.out.println("Total Emp days : " + totalWorkingDays + "  Emp Hr : " + empHrs);
         }
-        System.out.println("Total Emp days : " + totalWorkingDays);
         totalWage = totalEmpHrs * wagePerHour;
-        return  totalEmpHrs + totalWage;
+    }
+    @Override
+    public String toString(){
+        return "Total Emp wage for company : " + company + " is : " + totalWage;
     }
 
     public static void main(String[] args) {
+        System.out.println("Welcome to employee wage");
         EmpWage tata =new EmpWage("TATA",20,21,100);
-        System.out.println("Total Emp Hours of " + tata.company +" is " +tata.computeEmpWage());
-        System.out.println("Total Emp wage for  " + tata.company +" is " +tata.computeEmpWage() );
-        EmpWage honda =new EmpWage("Honda",10,30,150);
-        System.out.println("Total Emp Hours of " + honda.company +" is " +honda.computeEmpWage());
-        System.out.println("Total Emp wage for  " + honda.company +" is " +honda.computeEmpWage() );
+        tata.computeEmpWage();
+        System.out.println(tata);
     }
 }
